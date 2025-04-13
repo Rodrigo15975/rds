ae911770dfa750f0a7906b31365a1183dacc20c4
rodrigo15975 <javierluis15975@hotmail.com>
2025-04-12 23:56:09 -0500

    feat: add UserModule to AppModule and create UserController tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { UserModule } from './module/user/user.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
